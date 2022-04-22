c5d3714383f2ec4d8b202ee6d0029d6aa20d53b5
CsdSreekanta <sreekanta.csd@gmail.com>
2022-04-22 16:14:35 +0600

    click updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
+   
   const{img, name, price, seller, ratings} =props.product
+
+
+ 
     
     return (
         
@@ -14,7 +18,7 @@ const Product = (props) => {
                     <p><small>Manufacturer:{seller}</small></p>
                     <p><small>Rating:{ratings}</small></p>
             </div>
-            <button className='button-cart'>
+            <button onClick={() =>props.handleAddToCart(props.product)} className='button-cart'>
                 <p>Add to cart</p>
             </button>
         </div>
