78eb407abb0f5d38bb3d37073beca556aabd4f18
CsdSreekanta <sreekanta.csd@gmail.com>
2022-04-23 20:08:36 +0600

    cart information added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+
+    let total =0;
+    let shipping= 0;
+    let tax = 0
+    let grandTotal =0;
+
+    for(const product of cart){
+        total = total + product.price;
+        shipping= shipping +product.shipping;
+    }
+    tax = parseFloat((total*0.1).toFixed(2))
+    grandTotal = total+shipping+tax;
+    
     return (
-        <div>
+        <div className='cart'>
            <h2>Order Summary</h2>
             <p>Selected item: {cart.length}</p> 
+            <p>Total: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <p>Grand Total: ${grandTotal}</p>
         </div>
     );
 };
